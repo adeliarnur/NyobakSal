fb37a1936b3c057fb96d21fcb145b8076035b2b3
atmaranti <salsabilaatmaranti@yahoo.com>
2017-12-07 17:41:46 +0700

    first blood

--- app/src/main/java/com/example/asus/nyobaksal/MainActivity.java
+++ app/src/main/java/com/example/asus/nyobaksal/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.asus.nyobaksal;
 
 import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,23 +9,27 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
     ImageButton login;
     ImageButton signUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login = (ImageButton) findViewById(R.id.imageButton);
-        signUp = (ImageButton) findViewById(R.id.imageButton2);
+
+        login = (ImageButton) findViewById(R.id.Login2);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                Intent intent = new Intent(MainActivity.this, LoginActivity2.class);
+                startActivity(intent);
             }
         });
 
+        signUp = (ImageButton) findViewById(R.id.signup);
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SignUp.class));
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
             }
         });
        
